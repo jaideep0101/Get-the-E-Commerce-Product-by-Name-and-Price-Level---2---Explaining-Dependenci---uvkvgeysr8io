03a2bd67c8e9bcd9ab6deaad98a8eb78c3dcad74
Jaideep singh thakur <88130081+jaideep0101@users.noreply.github.com>
2023-04-22 12:47:28 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,5 +10,28 @@ app.use(express.json());
 
 // GET endpoint for sending the products to client by id
 //// Endpoint - /api/v1/products/:id
+app.get("/products/:id",(req,res)=>{
+const id = (req.params.id);
+  const product = products.find(p => p.id);
+  
+  if(product){
+  res.status(200).json({
+  status:"success",
+  message:"Product fetched successfully",
+    data:{
+    products:{
+    id:product.id,
+      name:product.name,
+      price:product.price,
+      quantity:product.quantity
+    }}
+  });
+  }else{
+    res.status(404).json({
+    status:"failed",
+    message:"Product not faound!"
+    })
+  }
+})
 
 module.exports = app;
